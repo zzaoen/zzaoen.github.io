d40bfb488e79480c72c436ef00c57665965dd440
zhzh402@163.com <zzhao3@stubhub.com>
2022-05-18 09:41:13 +0800

    change to vue theme

--- README.md
+++
@@ -1,2 +0,0 @@
-# zzaoen's blog
-
---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,31 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme";
+
+export default defineUserConfig({
+  base: "/",
+
+  head: [
+    [
+      "link",
+      {
+        rel: "stylesheet",
+        href: "//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
+      },
+    ],
+  ],
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Aoen's blog",
+      description: "Disk is always more reliable than memory",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "Aoen's blog",
+      description: "好记性不如烂笔头",
+    },
+  },
+
+  theme,
+});
